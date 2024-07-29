2277ef62eb535f320d27c8656a1cf99a5e3f8fca
Josh Waterson <jjwaterson@hotmail.co.uk>
2024-07-29 20:17:44 +0100

    initial commit

---
+++ src/main/java/com/medexpressconsultation/MedexpressConsultationApplication.java
@@ -0,0 +1,13 @@
+package com.medexpressconsultation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedexpressConsultationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MedexpressConsultationApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/medexpressconsultation/MedexpressConsultationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medexpressconsultation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedexpressConsultationApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
