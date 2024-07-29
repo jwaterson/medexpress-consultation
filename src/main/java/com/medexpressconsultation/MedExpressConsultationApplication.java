cdde7fbbcefb9b6b8d9e51e3eacdaee705e61eae
Josh Waterson <jjwaterson@hotmail.co.uk>
2024-07-29 21:34:54 +0100

    update class name - nitpicking

--- src/main/java/com/medexpressconsultation/MedExpressConsultationApplication.java
+++ src/main/java/com/medexpressconsultation/MedExpressConsultationApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MedexpressConsultationApplication {
+public class MedExpressConsultationApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(MedexpressConsultationApplication.class, args);
+    SpringApplication.run(MedExpressConsultationApplication.class, args);
   }
 
 }
